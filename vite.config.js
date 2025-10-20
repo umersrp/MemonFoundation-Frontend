47fb76d4cca648974d42faf84e0a24cc20449cac
unknown <artatkhan4@gmail.com>
2025-10-20 14:34:06 +0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
       values: {
         __DEV__: JSON.stringify(true),
         "process.env.NODE_ENV": JSON.stringify("development"),
-        "process.env.REACT_APP_BASE_URL": JSON.stringify("http://localhost:3200/api"),
+        "process.env.REACT_APP_BASE_URL": JSON.stringify("https://ksvvmxbk-3200.inc1.devtunnels.ms/api"),
       },
     }),
     react(),
